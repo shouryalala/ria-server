579d3cf40db81b17a25efbaa1af3b85742e9478f
shouryalala <shouryalala@gmail.com>
2018-10-28 12:25:33 +0530

    contains boiler code and 2 functions - basic notification and data packet notification

---
+++ functions/index.js
@@ -0,0 +1,126 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+exports.sendNotification = functions.https.onRequest((req, res) => {
+	const sent_text = req.query.text;
+	const promises = [];	
+	const getInstaceIdToSend = admin.database().ref('/users').once('value').then(function(snapshot) {	
+	//return Promise.all(admin.database().ref('/users').once('value')).then(function(snapshot) {	
+		const instanceId = snapshot.val();
+		console.log('notifying right here right now yo' + instanceId);
+
+		const payload = {
+			notification: {
+				title: "You've got work",
+				body: sent_text,
+				icon: "default"
+			}
+		};
+		return admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);                    
+                    return 1;
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                    return 0;
+                });
+	});
+
+});
+
+exports.sendDataPacket = functions.https.onRequest((req, res) => {
+	const service = req.query.service;
+	const address = req.query.address;
+	const time = req.query.time;
+	const promises = [];	
+	const getInstaceIdToSend = admin.database().ref('/users').once('value').then(function(snapshot) {	
+	//return Promise.all(admin.database().ref('/users').once('value')).then(function(snapshot) {	
+		const instanceId = snapshot.val();
+		console.log('sending data packet as a notification' + instanceId);
+
+		const payload = {
+			data: {
+				Service: service,
+				Address: address,
+				Time: time,
+				shourya: "lala"
+			}
+		};
+		return admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);                    
+                    return 1;
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                    return 0;
+                });
+	});
+
+});
+
+
+
+/*
+exports.sendNotification = functions.database.ref('/notifications/messages/{pushId}')
+    .onWrite(event => {
+        const message = event.data.current.val();
+        const senderUid = message.from;
+        const receiverUid = message.to;
+        const promises = [];
+
+        if (senderUid == receiverUid) {
+            //if sender is receiver, don't send notification
+            promises.push(event.data.current.ref.remove());
+            return Promise.all(promises);
+        }
+
+        const getInstanceIdPromise = admin.database().ref(`/users/${receiverUid}/instanceId`).once('value');
+        const getSenderUidPromise = admin.auth().getUser(senderUid);
+
+        return Promise.all([getInstanceIdPromise, getSenderUidPromise]).then(results => {
+            const instanceId = results[0].val();
+            const sender = results[1];
+            console.log('notifying ' + receiverUid + ' about ' + message.body + ' from ' + senderUid);
+
+            const payload = {
+                notification: {
+                    title: sender.displayName,
+                    body: message.body,
+                    icon: sender.photoURL
+                }
+            };
+
+            admin.messaging().sendToDevice(instanceId, payload)
+                .then(function (response) {
+                    console.log("Successfully sent message:", response);
+                })
+                .catch(function (error) {
+                    console.log("Error sending message:", error);
+                });
+        });
+});*/
+
+/*
+//PROMISE SUCCESSON AND REJECTION
+
+ref.child('blogposts').child(id).once('value').then(function(snapshot) {
+  // The Promise was "fulfilled" (it succeeded).
+  renderBlog(snapshot.val());
+}, function(error) {
+  // The Promise was rejected.
+  console.error(error);
+});
+*/
