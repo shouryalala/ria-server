19e4260eba382cde0f52aa201e7ff13f978b30c7
shouryalala <shouryalala@gmail.com>
2019-03-04 23:48:09 +0530

    added dummy request creation function. Previous functionality TESTED

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,20 @@
 const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 
 //var serviceAccount = require('kanta-6f9f5-firebase-adminsdk-53vmc-c7119079df.json');
 //firebase-adminsdk-53vmc@kanta-6f9f5.iam.gserviceaccount.com
 
+/**
+ * Terminal commands
+ * npm run-script lint
+ * firebase deploy --only functions
+ * firebase functions:log --only {function name} 
+ */
+
 //OLD
 //admin.initializeApp(functions.config().firebase);
-//NEw
+//NEW
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: 'kanta-6f9f5',
@@ -25,6 +24,21 @@ admin.initializeApp({
 
   databaseURL: 'https://kanta-6f9f5.firebaseio.com'
 });
+
+//Firebase db fields
+const AST_TOKEN = "client_token";
+const AST_TOKEN_TIMESTAMP = "ct_update_tmstmp";
+const REQ_STATUS_ASSIGNED = "ASN";
+const REQ_STATUS_UNASSIGNED = "UNA";
+const AST_RESPONSE_NIL = "NIL";
+const AST_RESPONSE_ACCEPT = "ACCEPT";
+const AST_RESPONSE_REJECT = "REJECT";
+const COMMAND_WORK_REQUEST = "WRDP";
+//Firebase collections
+const COLN_USERS = "users";
+const COLN_ASSISTANTS = "assistants";
+const COL_REQUEST = "requests";
+
 
 /*
 * createToken method - accepts mobile number and returns a custom JWT token for authentication
@@ -87,7 +101,6 @@ exports.sendNotification = functions.https.onRequest((req, res) => {
     end();
 });
 
-
 //BOILERPLATE FUNCTION
 exports.sendDataPacket = functions.https.onRequest((req, res) => {
 	const service = req.query.service;
@@ -122,14 +135,16 @@ exports.sendDataPacket = functions.https.onRequest((req, res) => {
 
 });
 
-
 //BOILERPLATE FUNCTION
 exports.sendDataPacketFixedClient = functions.https.onRequest((req, res) => {
 	const service = req.query.service;
 	const address = req.query.address;
 	const time = req.query.time;
 	//const priority = req.query.priority;
-    const promises = [];	
+    const promises = [];
+
+
+
     const instanceId = "eI_G6yeMYKM:APA91bEFa8OHe7l67ZO8Es5zgVVeIUKo6ok_BSg-MmB1H5YDBZYTt8_BOM_IwlT1aH6ilTWHsvFrUKglD3ImXrrajS4x0J6C0i26_xZCgMKAOmkwTMajuKFYDPkj2fFrIIMx0tduhwMW";
 	
 	console.log('sending data packet to client: ' + instanceId);
@@ -139,21 +154,40 @@ exports.sendDataPacketFixedClient = functions.https.onRequest((req, res) => {
             Service: service,
             Address: address,
             Time: time,
-            Command: WRDP
+            Command: COMMAND_WORK_REQUEST
             //Priority: priority
         }
     };
     return admin.messaging().sendToDevice(instanceId, payload)
             .then(function (response) {
                 console.log("Successfully sent message:", response);
-                return 1;
+                return res.status(200).send("ok");
             })
             .catch(function (error) {
                 console.log("Error sending message:", error);
-                return 0;
+                return res.status(500).send(error);
             });
 });
 
+
+exports.createDummyRequest = functions.https.onRequest((req, res) => {
+    var packet = {
+        address:"sexy legs",
+        asn_response: "NIL",
+        service: "CLN",
+        society_id: "we dont need no education",
+        status: "UNA",
+        user_id: "9986643444",
+        requested_time: "12:32"
+    }
+
+    return admin.firestore().collection(COL_REQUEST).add(packet).then(() => {
+        return res.status(200).send("Created automagically!");
+    })
+    .catch((error) => {
+        return res.status(500).send("Error: " + error);
+    });
+});
 
 
 /**
@@ -170,37 +204,42 @@ exports.userRequestHandler = functions.firestore
         console.log("::userRequestHandler::INVOKED");
         const requestObj = snap.data();
         const requestId = context.params.requestId;     //get request ID from wildcard
-        /*
-        assistant should contain:
-        {assistant instance ID, assistant name, assistant ID}
-        */
-        var assistant = getAvailableAssistant(requestObj.address, requestObj.service, requestObj.time, null);
-        console.log("Request Id: "+ requestId + "\nSelected assistant: " + assistant.name);
-
-        if(!assistant) {
-            console.log("No available maids at the moment.");
-            //TODO
-            return 0;
-        }
-        //TODO NO NULL CHECKS ANYWHERE
-
-        //sendAssitantRequest(requestObj, assistant)
-        return sendAssitantRequest(requestId, requestObj, assistant)
-            .then(function(response){
-                if(response === 1) {
-                    console.log("Updating the snapshot's assignee.");
-                    return snap.ref.set({
-                        assignee_id: assistant.ass_id,
-                        asn_response: "NIL"     //Can be set by client
-                    }, {merge: true});
-                }else{
-                    console.log("Recevied error tag from :sendAssistantRequest: method");
+        
+        return getAvailableAssistant(requestObj.address, requestObj.service, requestObj.time, null)
+            .then(function(assistant){
+                /*
+                    assistant should contain: 
+                    {assistant client token, assistant name, assistant ID}
+                */
+                if(assistant === null) {
+                    console.log("No available maids at the moment.");
+                    //TODO
                     return 0;
                 }
-        }, function(error){
-            console.log("Recevied error tag from :sendAssistantRequest: method");
-            return 0;
-        });
+               
+                console.log("Request Id: "+ requestId + "\nSelected assistant: " + assistant.name);
+                
+                //TODO NO NULL CHECKS ANYWHERE
+        
+                //sendAssitantRequest(requestObj, assistant)
+                return sendAssitantRequest(requestId, requestObj, assistant)
+                    .then(function(response){
+                        if(response === 1) {
+                            console.log("Updating the snapshot's assignee.");
+                            return snap.ref.set({
+                                assignee_id: assistant.assId,
+                                asn_response: "NIL"     //Can be set by client
+                            }, {merge: true});
+                        }else{
+                            console.log("Recevied error tag from :sendAssistantRequest: method");
+                            return 0;
+                        }
+                }, function(error){
+                    console.log("Recevied error tag from :sendAssistantRequest: method" + error);
+                    return 0;
+                });
+
+            });       
     });
 
 
@@ -215,23 +254,23 @@ var sendAssitantRequest = function(requestId, request, assistant) {
             RID: requestId,
             Service: request.service,
             Address: request.address,
-            Time: request.time,
-            Command: "WRDP"
+            Time: request.requested_time,
+            Command: COMMAND_WORK_REQUEST
         }
     };
 
     console.log("Attempting to send the request to an available assistant..");
     //send payload to assistant        
-    return admin.messaging().sendToDevice(assistant.instanceId, payload)
+    return admin.messaging().sendToDevice(assistant.clientToken, payload)
             .then(function(response) {
                 console.log("Request sent succesfully.");
                 setTimeout(() => {
                     checkRequestStatus(requestId, assistant.assId);
-                }, 4*60*1000);
+                }, 2*60*1000);
                 return 1;
             })
             .catch(function(error) {
-                console.log("Request couldnt be sent.");
+                console.log("Request couldnt be sent." + error);
                 //TODO
                 return 0;
             });
@@ -248,6 +287,7 @@ var sendAssitantRequest = function(requestId, request, assistant) {
     REJECTED
     */
 
+//BE SUPER CAREFUL OF SELF LOOPING
 exports.assistantResponseHandler = functions.firestore
     .document('request/{requestId}')
     .onUpdate((change, context) => {
@@ -260,23 +300,23 @@ exports.assistantResponseHandler = functions.firestore
         console.log("RequestId: " + requestId + "\nResponse before: " + response_before + " Response after: " + response_after);        
         
         //CASE 1
-        if(status_after === "CONFIRMED" || response_before === response_after) {
+        if(status_after === REQ_STATUS_ASSIGNED || response_before === response_after) {
             //exit if no changes to response or if request already confirmed
             console.log("No change to response || status is confirmed. Exiting method.");
             return 0;
         }
         
-        if(response_before === "NIL") {
+        if(response_before === AST_RESPONSE_NIL) {
             //CASE 2
-            if(response_after === "ACCEPTED") {
+            if(response_after === AST_RESPONSE_ACCEPT) {
                 console.log("Request accepted. Confirming request and notifying user");
                 //TODO  update assistant object. update request status. notify user
                 return change.ref.set({                    
-                    status: "ASN"     //looping handled
-                }, {merge: true});           
+                    status: REQ_STATUS_ASSIGNED     //looping handled
+                }, {merge: true});
             }
             //CASE 3
-            else if(response_after === "REJECTED") {
+            else if(response_after === AST_RESPONSE_REJECT) {
                 console.log("Request rejected. Reroute request");
                 //TODO
                 return 0;
@@ -296,13 +336,25 @@ exports.assistantResponseHandler = functions.firestore
  * return assistant = {name, instanceId, assId}
  */
 var getAvailableAssistant = function(address, service, time, exceptions) {
-    console.log("::getAvailableAssistant::INVOKED");
-    var dummy = {
-        name: "Sushma",
-        assId: "ADD_IN_BRO",
-        instanceId: "ADD_IN_BRO"
-    };
-    return dummy;
+    console.log("::getAvailableAssistant::INVOKED");    
+
+    //TODO
+    var aId = "alYssfTuB2Y1tTw5jEaQfVCxUhX2";
+
+    return admin.firestore().collection(COLN_ASSISTANTS).doc(aId).get().then(doc => {
+        const aDoc = doc.data();
+        console.log("Fetched Available assitant details: Assistant name: " + aDoc.name + " ID: " + aId)
+        var assistant = {
+            name: aDoc.name,
+            assId: aId,
+            clientToken: aDoc.client_token
+        }
+        return assistant;
+    })
+    .catch(error => {
+        console.log("Error fetching record." + error);
+        return null;
+    });   
 }
 
 /**
@@ -315,12 +367,14 @@ var getAvailableAssistant = function(address, service, time, exceptions) {
  */
 var checkRequestStatus = function(rId, assId) {
     console.log("::checkRequestStatus::INVOKED");
-    admin.firestore().collection('requests').doc(rId).get().then(doc => {        
-        if(doc.assignee_id === assId && doc.asn_response === "NIL") {
+    admin.firestore().collection(COL_REQUEST).doc(rId).get().then(doc => {        
+        if(doc.assignee_id !== null && doc.asn_response != null && doc.assignee_id === assId && doc.asn_response === AST_RESPONSE_NIL) {
             //the assigned assistant is still the same and the her response is still nil
             //Houston we have a problem
             console.log("Request hasnt been responded to by assistant. Required to be rerouted");
             //TODO      
+        }else{
+            console.log("Fetched record, condition not satisfied.");
         }
         return 1;
     })
